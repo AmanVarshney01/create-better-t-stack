18a34b4bfde234a9e8dd612ab4cfb58c2b13189a
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-03-24 19:36:38 +0100

    Fix Postgres DB connection

--- apps/cli/template/with-drizzle-postgres/apps/server/src/db/index.ts
+++ apps/cli/template/with-drizzle-postgres/apps/server/src/db/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const queryClient = postgres(process.env.DATABASE_URL);
-const db = drizzle({ client: queryClient });
+export const db = drizzle({ client: queryClient });
