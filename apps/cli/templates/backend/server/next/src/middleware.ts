f2bd595e02eafe8473ad4da939abe616ba19dd99
Joydip Roy <joydipand@gmail.com>
2025-05-17 21:43:12 +0530

    fix(issues-218): allow multiple cors origin on next.js server

--- apps/cli/templates/backend/server/next/src/middleware.ts
+++ apps/cli/templates/backend/server/next/src/middleware.ts
@@ -1,10 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware() {
+export function middleware(request: NextRequest) {
+  const originHeader = request.headers.get("origin");
   const res = NextResponse.next()
 
+  const allowedOrigins = process.env.CORS_ORIGINS?.split(",") ?? [];
+  if (originHeader && allowedOrigins.includes(originHeader)) {
+    res.headers.append("Access-Control-Allow-Origin", originHeader);
+  }
   res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || "")
   res.headers.append('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
   res.headers.append(
     'Access-Control-Allow-Headers',
