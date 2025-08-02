4f8c16b0a9d60a753139e454ac6512960832164e
Aman Varshney <amanvarshney.work@gmail.com>
2025-08-02 06:28:35 +0530

    ok

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -1,3 +1,5 @@
+// The website is currently deployed to Vercel you can ignore this file.
+
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig();
