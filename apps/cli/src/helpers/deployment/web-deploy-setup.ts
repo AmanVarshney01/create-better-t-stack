cdad96ed07494c45b42d9be55e43d61bbec71717
Aman Varshney <amanvarshney.work@gmail.com>
2025-08-18 11:38:52 +0530

    refactor next setup

--- apps/cli/src/helpers/deployment/web-deploy-setup.ts
+++ apps/cli/src/helpers/deployment/web-deploy-setup.ts
@@ -1,7 +1,6 @@
 import path from "node:path";
 import fs from "fs-extra";
 import type { PackageManager, ProjectConfig } from "../../types";
-import { addPackageDependency } from "../../utils/add-package-deps";
 import { setupCombinedAlchemyDeploy } from "./alchemy/alchemy-combined-setup";
 import { setupNextAlchemyDeploy } from "./alchemy/alchemy-next-setup";
 import { setupNuxtAlchemyDeploy } from "./alchemy/alchemy-nuxt-setup";
@@ -10,6 +9,7 @@ import { setupSolidAlchemyDeploy } from "./alchemy/alchemy-solid-setup";
 import { setupSvelteAlchemyDeploy } from "./alchemy/alchemy-svelte-setup";
 import { setupTanStackRouterAlchemyDeploy } from "./alchemy/alchemy-tanstack-router-setup";
 import { setupTanStackStartAlchemyDeploy } from "./alchemy/alchemy-tanstack-start-setup";
+import { setupNextWorkersDeploy } from "./workers/workers-next-setup";
 import { setupNuxtWorkersDeploy } from "./workers/workers-nuxt-setup";
 import { setupSvelteWorkersDeploy } from "./workers/workers-svelte-setup";
 import { setupTanstackStartWorkersDeploy } from "./workers/workers-tanstack-start-setup";
@@ -91,34 +91,4 @@ async function setupWorkersWebDeploy(
 	}
 
 	await setupWorkersVitePlugin(projectDir);
-}
-
-async function setupNextWorkersDeploy(
-	projectDir: string,
-	_packageManager: PackageManager,
-) {
-	const webAppDir = path.join(projectDir, "apps/web");
-	if (!(await fs.pathExists(webAppDir))) return;
-
-	await addPackageDependency({
-		dependencies: ["@opennextjs/cloudflare"],
-		devDependencies: ["wrangler"],
-		projectDir: webAppDir,
-	});
-
-	const packageJsonPath = path.join(webAppDir, "package.json");
-	if (await fs.pathExists(packageJsonPath)) {
-		const pkg = await fs.readJson(packageJsonPath);
-
-		pkg.scripts = {
-			...pkg.scripts,
-			preview: "opennextjs-cloudflare build && opennextjs-cloudflare preview",
-			deploy: "opennextjs-cloudflare build && opennextjs-cloudflare deploy",
-			upload: "opennextjs-cloudflare build && opennextjs-cloudflare upload",
-			"cf-typegen":
-				"wrangler types --env-interface CloudflareEnv cloudflare-env.d.ts",
-		};
-
-		await fs.writeJson(packageJsonPath, pkg, { spaces: 2 });
-	}
 }
